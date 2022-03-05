a12c0bbdea54425707b959a78c2db7ca01e52e31
Blake Bennett <babennettdev@gmail.com>
2022-03-05 11:13:01 -0500

    Updating param key name

--- src/titanic/helpers/one_hot_encode_passenger_sex.py
+++ src/titanic/helpers/one_hot_encode_passenger_sex.py
@@ -4,7 +4,7 @@ import pandas as pd
 def one_hot_encode_passenger_sex(
     *, 
     one_hot_encoder: OneHotEncoder,
-    titanic_train_dataframes: pd.DataFrame
+    titanic_dataframes: pd.DataFrame
 ) -> pd.DataFrame: 
     """
     Function to one hot encode passenger sex ("male" or "female").
@@ -19,11 +19,11 @@ def one_hot_encode_passenger_sex(
 
     Return
     ----------
-    titanic_train_dataframes: pandas.DataFrame
+    titanic_dataframes: pandas.DataFrame
     """
     
-    titanic_train_sex_dataframes = one_hot_encoder.fit_transform(titanic_train_dataframes["Sex"].values.reshape(-1, 1))
+    titanic_train_sex_dataframes = one_hot_encoder.fit_transform(titanic_dataframes["Sex"].values.reshape(-1, 1))
     titanic_train_sex_dataframes.columns = one_hot_encoder.get_feature_names(['Sex'])
     titanic_train_sex_dataframes = pd.DataFrame(titanic_train_sex_dataframes.toarray(), columns=titanic_train_sex_dataframes.columns)
-    titanic_train_dataframes= pd.concat([titanic_train_dataframes, titanic_train_sex_dataframes ], axis=1)
-    return titanic_train_dataframes
+    titanic_dataframes= pd.concat([titanic_dataframes, titanic_train_sex_dataframes ], axis=1)
+    return titanic_dataframes
