9a047d780208f2d9db35c3cdfa753e6099e48b34
Blake Bennett <babennettdev@gmail.com>
2022-03-13 03:22:44 -0400

    Updated fill_na_with_mean() to use mode for non-numerics

--- src/helpers/fill_na_with_mean.py
+++ src/helpers/fill_na_with_mean.py
@@ -1,5 +1,5 @@
 from typing import List
-from numpy import ndarray, number
+import numpy as np
 import pandas as pd
 
 def fill_na_with_mean(dataframe: pd.DataFrame, columns_to_fill: List) -> float:
@@ -16,6 +16,11 @@ def fill_na_with_mean(dataframe: pd.DataFrame, columns_to_fill: List) -> float:
     filled_dataframe : pandas.DataFrame
     """
     for column_to_fill in columns_to_fill:
-        dataframe[column_to_fill].fillna(dataframe[column_to_fill].mean(),inplace=True)
+        if dataframe[column_to_fill].dtype == 'float' or dataframe[column_to_fill].dtype == 'float64' \
+            or dataframe[column_to_fill].dtype == 'int' or dataframe[column_to_fill].dtype == 'int64':
+                dataframe[column_to_fill].fillna(dataframe[column_to_fill].mean(),inplace=True) 
+        else:
+            dataframe[column_to_fill].replace('NaN',np.NaN,inplace=True)
+            dataframe[column_to_fill].fillna(dataframe[column_to_fill].mode()[0],inplace=True)
     
     return dataframe
